7eba5b1c381cd22a3a6daa2c778252b51c8507c0
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-11-23 14:42:23 +0100

    Terraform in CI first try

--- ops/terraform/envs/staging/backend.tf
+++ ops/terraform/envs/staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "webgrip-terraform-state-0002"   # Replace with the bucket name from bootstrap output
-    key            = "${var.environment}/terraform.tfstate"
+    key            = "staging/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "webgrip-terraform-lock-0002"  # Replace with the table name from bootstrap output
     encrypt        = true
